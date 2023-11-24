a0283ed8bd5ad49b49cd1cf6a4b27190ad07aa82
macbook <taras.i.panteliuk@ukd.edu.ua>
2023-11-24 15:55:30 +0200

    modified:   .DS_Store 	modified:   images/.DS_Store 	new file:   images/photo_2023-11-21_19.09.21-removebg-preview.png 	new file:   images/photo_2023-11-21_19.09.24-removebg-preview.png

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    var menu = document.getElementById("navMenu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function closeMenu() {
+    var menu = document.getElementById("navMenu");
+    menu.style.display = "none";
+}
+
+
+
+
