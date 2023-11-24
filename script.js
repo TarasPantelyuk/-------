b585950025d640a1fd635283345434447d69ce64
macbook <taras.i.panteliuk@ukd.edu.ua>
2023-11-24 17:52:13 +0200

    modified:   .DS_Store 	modified:   index.html 	new file:   order.html 	new file:   orderindex.html 	modified:   script.js     new file:   server.js 	modified:   style.css

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ function closeMenu() {
     menu.style.display = "none";
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const orderForm = document.getElementById('orderForm');
+    const resultDiv = document.getElementById('result');
+  
+    orderForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+  
+      // Отримання даних з форми
+      const formData = new FormData(orderForm);
+  
+      // Ваш код для обробки форми, наприклад, відправка даних на сервер за допомогою AJAX або Fetch
+  
+      // Приклад виведення результату на сторінці
+      resultDiv.innerHTML = '<p>Замовлення успішно відправлено!</p>';
+    });
+  });
+  
 
 
 
