5687d3291a8d030dd70a21c0ad89e7208066c61c
macbook <taras.i.panteliuk@ukd.edu.ua>
2023-11-29 20:23:17 +0200

    modified:   .DS_Store 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function closeMenu() {
     var menu = document.getElementById("navMenu");
     menu.style.display = "none";
 };
+function resizeImage() {
+    var image = document.querySelector(".background-image");
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    image.style.width = width;
+    image.style.height = height;
+  }
+  
+  window.addEventListener("resize", resizeImage);
